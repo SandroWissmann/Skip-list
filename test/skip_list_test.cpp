4089b1213ae85564b540252d0df3b3a274045ec2
SandroWissmann <sandro.wissmann@googlemail.com>
2020-07-01 18:20:02 +0200

    test: adds test for move assignment

--- test/skip_list_test.cpp
+++ test/skip_list_test.cpp
@@ -152,6 +152,23 @@ TEST(Skip_list, move_constructor)
     EXPECT_TRUE(obj.empty());
 }
 
+TEST(Skip_list, move_asignment)
+{
+    Skip_list<int, int> obj1;
+    obj1.insert(std::make_pair(1, 10));
+
+    Skip_list<int, int> obj2;
+    obj2.insert(std::make_pair(2, 20));
+
+    EXPECT_EQ(obj1[1], 10);
+    EXPECT_EQ(obj2[2], 20);
+
+    obj2 = std::move(obj1);
+
+    EXPECT_TRUE(obj1.empty());
+    EXPECT_EQ(obj2.find(2), obj2.end());
+}
+
 
 TEST(Skip_list, empty)
 {
